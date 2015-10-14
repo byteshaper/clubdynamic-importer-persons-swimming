c2578e76b061467da89d19b439d23a36f700cbeb
Henning Schuetz <henning@byteshaper.com>
2015-10-15 00:19:00 +0200

    started implementing line parser, test-driven, of course ;)

--- src/main/java/com/clubdynamic/addons/importer/persons/swimming/LineParser.java
+++ src/main/java/com/clubdynamic/addons/importer/persons/swimming/LineParser.java
@@ -1,10 +1,33 @@
 package com.clubdynamic.addons.importer.persons.swimming;
 
 import com.clubdynamic.dto.person.PersonWriteDto;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class LineParser {
 
+  private static final String FIELD_SEPARATOR = "\\|";
+  private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+  
+  private static final int COL_LASTNAME = 0;
+  private static final int COL_FIRSTNAME = 1;
+  private static final int COL_BIRTHDAY = 2;
+  private static final int COL_GENDER = 3;
+  private static final int COL_STREET = 4;
+  private static final int COL_ZIP = 5;
+  private static final int COL_CITY = 6;
+
   public PersonWriteDto parse(String line) {
-    return null;
+    PersonWriteDto person = new PersonWriteDto();
+    String[] lines = line.split(FIELD_SEPARATOR);
+    // System.err.println(line);
+    person.firstName = lines[COL_FIRSTNAME];
+    person.lastName = lines[COL_LASTNAME];
+    person.birthday = LocalDate.parse(lines[COL_BIRTHDAY], DATE_FORMATTER).toString();
+    person.gender = lines[COL_GENDER];
+    person.street = lines[COL_STREET];
+    person.zip = lines[COL_ZIP];
+    person.city = lines[COL_CITY];
+    return person;
   }
 }
---
+++ src/test/java/com/clubdynamic/addons/importer/persons/swimming/LineParserTest.java
@@ -0,0 +1,43 @@
+package com.clubdynamic.addons.importer.persons.swimming;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import com.clubdynamic.dto.person.PersonWriteDto;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LineParserTest {
+
+  private static List<String> csvLines;
+
+  private LineParser lineParser;
+
+  @BeforeClass
+  public static void readCsvLines() throws Exception {
+    csvLines =
+        Files.readAllLines(Paths.get(LineParserTest.class.getClassLoader().getResource("persons.csv").toURI()));
+  }
+
+  @Before
+  public void init() {
+    lineParser = new LineParser();
+  }
+
+  @Test
+  public void normalLineWithMemberAndLandline() {
+    PersonWriteDto person = lineParser.parse(csvLines.get(1));
+    assertThat(person.firstName, equalTo("Martina"));
+    assertThat(person.lastName, equalTo("Musterfrau"));
+    assertThat(person.birthday, equalTo("1967-05-30"));
+    assertThat(person.gender, equalTo("F"));
+    assertThat(person.street, equalTo("Musterstraße 1"));
+    assertThat(person.zip, equalTo("12345"));
+    assertThat(person.city, equalTo("Berlin"));
+  }
+}
+
